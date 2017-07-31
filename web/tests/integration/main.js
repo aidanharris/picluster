4bcbfdae0c98dd8f6ed1d9bf30b8809213b7b0a1
Aidan Harris <mail@aidanharris.io>
2017-07-31 08:34:35 +0100

    Test jQuery UI version

--- web/tests/integration/main.js
+++ web/tests/integration/main.js
@@ -1,4 +1,4 @@
-casper.test.begin('main.html', 6, function(test) {
+casper.test.begin('main.html', 7, function(test) {
   const URL = casper.cli.get('url');
   const username = casper.cli.get('username');
   const password = casper.cli.get('password');
@@ -21,9 +21,19 @@ casper.test.begin('main.html', 6, function(test) {
       test.assertEquals(1, iframe.length, "There should be 1 iframe");
       test.assertEquals(iframe.url, URL + '/blank', "Its source should equal " + URL + "/blank");
 
-      this.wait(2000, function() {
+      this.waitForResource("jquery.min.js", function() {
         var jquery = this.evaluate(function() {
           return $.fn.jquery;
+        });
+
+        const expected_jqueryUI = "1.12.1";
+        var jqueryUI;
+        this.waitForResource("jquery-ui.js", function() {
+          jqueryUI = this.evaluate(function() {
+            return $.ui.version;
+          });
+
+          test.assertEquals(jqueryUI, expected_jqueryUI, "jQuery UI should be '" + expected_jqueryUI + "'");
         });
 
         test.assert(expected_jquery.indexOf(jquery) > -1, "jQuery should be " + expected_jquery);
